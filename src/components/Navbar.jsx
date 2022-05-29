b0f2f55884afd404b0b2aa24e10a7af7f5049e1e
Najeeb Farhan <najeebfarhan6345@gmail.com>
2022-05-29 18:46:04 +0530

    i call it a day

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <nav className="py-5 flex justify-center shadow-md">
+        <nav className="py-5 flex justify-center shadow-md sticky top-0 bg-white z-50">
             <div className="mx-5 w-full max-w-screen-xl flex justify-between">
             <h1 className="text-lg font-semibold">Where in the world?</h1>
             <span className="font-thin">Dark Mode</span>
