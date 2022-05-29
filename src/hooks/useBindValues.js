66bc6d4f32566a58ace79e345f073107be11647d
Najeeb Farhan <najeebfarhan6345@gmail.com>
2022-05-29 19:32:37 +0530

    created useBindValues hook

---
+++ src/hooks/useBindValues.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const useBindValues = (arr) => {
+  const [values, setValues] = useState(arr || []);
+  function handleValues(value) {
+    return (e) => {
+      const isChecked = e.target.checked;
+      isChecked
+        ? setValues([...values, value])
+        : setValues(values.filter((val) => val !== value));
+    };
+  }
+  return values, handleValues;
+};
+
+export default useBindValues;
